f73d82fafeeeae6ea65cd25a9ef5267a40578450
Lendwye <d.gantsev358@gmail.com>
2023-02-15 20:42:17 +0300

    Configuring to deploy on server

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+				'resources/js/js_vue/post_component.js',
+				'resources/js/js_vue/post_page_component.js',
+				'resources/js/js_vue/user_component.js',
+				'resources/js/js_vue/user_page_component.js',
             ],
             refresh: true,
         }),
